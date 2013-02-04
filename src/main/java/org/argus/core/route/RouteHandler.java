d087c297a69ec97eaf69cc1e2514881567c0beb9
Cheng <cheng@Amber.(none)>
2013-02-04 23:20:18 +0800

    init

---
+++ src/main/java/org/argus/core/route/RouteHandler.java
@@ -0,0 +1,11 @@
+package org.argus.core.route;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RouteHandler {
+
+	public void get(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("success");
+	}
+}
